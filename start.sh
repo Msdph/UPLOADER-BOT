c081ce7cdf6177601777dc386bfb50310c2cc051
Msdph <99562192+Msdph@users.noreply.github.com>
2022-07-25 15:52:33 +0430

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Tellybots/Uploader-Bot.git /Uploader-Bot     
+  git clone https://github.com/Msdph/Uploader-Bot.git /Uploader-Bot     
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Uploader-Bot
